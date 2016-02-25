268e19769c524bd001acf889d39f6f080374e6d5
Scott Todd <scott.todd0@gmail.com>
2016-02-24 20:55:14 -0800

    Added a test for including the Vulkan SDK.

---
+++ src/test_vulkan_sdk_include.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#include <vulkan/vulkan.h>
+
+int main() {
+  std::cout << "Hello!" << std::endl;
+
+  std::cout << "VkFormat.VK_FORMAT_R8_SNORM: " << VK_FORMAT_R8_SNORM
+            << std::endl;
+}
